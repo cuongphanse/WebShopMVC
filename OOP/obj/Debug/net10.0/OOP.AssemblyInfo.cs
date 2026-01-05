5b525e24ddabaf8b1fbbee5385be5fe6731d34fe
cuongphan <cuongpvpvc@gmail.com>
2026-01-05 16:50:08 +0700

    remove smtp

--- OOP/obj/Debug/net10.0/OOP.AssemblyInfo.cs
+++ OOP/obj/Debug/net10.0/OOP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+043a1b5b44001281450b6f4010cda34d5188b790")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49564da8027f42042fda42dd4e70d47da848d127")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
